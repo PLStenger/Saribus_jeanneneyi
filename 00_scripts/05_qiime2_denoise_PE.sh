347de339c46e3eb47ea8dcb438c37075cee7d8ea
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-11-23 11:53:57 +1100

    Update 05_qiime2_denoise_PE.sh

--- 00_scripts/05_qiime2_denoise_PE.sh
+++ 00_scripts/05_qiime2_denoise_PE.sh
@@ -175,29 +175,29 @@ conda activate qiime2-2021.4
 
 # https://github.com/benjjneb/dada2/issues/477
 
-#qiime dada2 denoise-paired --i-demultiplexed-seqs core/demux.qza \
-#--o-table core/Table.qza  \
-#--o-representative-sequences core/RepSeq.qza \
-#--o-denoising-stats core/Stats.qza \
-#--p-trim-left-f 0 \
-#--p-trim-left-r 0 \
-#--p-trunc-len-f 0 \
-#--p-trunc-len-r 0 \
-#--p-n-threads 4  
-#
+qiime dada2 denoise-paired --i-demultiplexed-seqs core/demux.qza \
+--o-table core/Table.qza  \
+--o-representative-sequences core/RepSeq.qza \
+--o-denoising-stats core/Stats.qza \
+--p-trim-left-f 0 \
+--p-trim-left-r 0 \
+--p-trunc-len-f 0 \
+--p-trunc-len-r 0 \
+--p-n-threads 4  
+
 ###################################################################
 ## Negative control
 ###################################################################
-#qiime dada2 denoise-paired --i-demultiplexed-seqs core/demux_neg.qza \
-#--o-table core/Table_neg.qza  \
-#--o-representative-sequences core/RepSeq_neg.qza \
-#--o-denoising-stats core/Stats_neg.qza \
-#--p-trim-left-f 0 \
-#--p-trim-left-r 0 \
-#--p-trunc-len-f 0 \
-#--p-trunc-len-r 0 \
-#--p-n-threads 4   
-#
+qiime dada2 denoise-paired --i-demultiplexed-seqs core/demux_neg.qza \
+--o-table core/Table_neg.qza  \
+--o-representative-sequences core/RepSeq_neg.qza \
+--o-denoising-stats core/Stats_neg.qza \
+--p-trim-left-f 0 \
+--p-trim-left-r 0 \
+--p-trunc-len-f 0 \
+--p-trunc-len-r 0 \
+--p-n-threads 4   
+
 # sequence_contamination_filter :
 #################################
 
