311c67a612d6bb92ea3d3adc5142880709706cd9
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2022-02-08 16:28:33 +1100

    Create 09_core_biom_PE.sh

---
+++ 00_scripts/09_core_biom_PE.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+# pathways in cluster:
+DATADIRECTORY_ITS=/scratch_vol1/fungi/Saribus_jeanneneyi/05_QIIME2/ITS/
+DATADIRECTORY_16S=/scratch_vol1/fungi/Saribus_jeanneneyi/05_QIIME2/16S/
+
+METADATA_ITS=/scratch_vol1/fungi/Saribus_jeanneneyi/98_database_files/ITS/
+METADATA_16S=/scratch_vol1/fungi/Saribus_jeanneneyi/98_database_files/16S/
+
+# pathways in local:
+#DATADIRECTORY_ITS=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/21_Saribus/Saribus_jeanneneyi/05_QIIME2/ITS/
+#DATADIRECTORY_16S=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/21_Saribus/Saribus_jeanneneyi/05_QIIME2/16S/
+
+#METADATA_ITS=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/21_Saribus/Saribus_jeanneneyi/98_database_files/ITS/
+#METADATA_16S=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/21_Saribus/Saribus_jeanneneyi/98_database_files/16S/
+
+
+
+###############################################################
+### For Fungi
+###############################################################
+
+cd $DATADIRECTORY_ITS
+
+eval "$(conda shell.bash hook)"
+conda activate qiime2-2021.4
+
+# Aim: Filter sample from table based on a feature table or metadata
+
+qiime feature-table filter-samples \
+        --i-table core/RarTable.qza \
+        --m-metadata-file $METADATA_ITS/sample-metadata.tsv \
+        --p-where "[#SampleID] IN ('bark_foret_nord_01', 'bark_foret_nord_02', 'bark_foret_nord_03', 'base_palm_creek_pernod', 'leaf_creek_pernod', 'leaf_nursery_01', 'leaf_nursery_02', 'leaf_PPRB_01', 'leaf_PPRB_02', 'root_creek_pernod')"  \
+        --o-filtered-table core/RarTable-all.qza
+
+        
+# Aim: Identify "core" features, which are features observed,
+     # in a user-defined fraction of the samples
+
+        
+qiime feature-table core-features \
+        --i-table core/RarTable-all.qza \
+        --p-min-fraction 0.1 \
+        --p-max-fraction 1.0 \
+        --p-steps 10 \
+        --o-visualization visual/CoreBiom-all.qzv  
+
+qiime tools export --input-path core/RarTable-all.qza --output-path export/core/RarTable-all    
+        
+#qiime tools export --input-path visual/CoreBiomAll.qzv --output-path export/visual/CoreBiomAll
+qiime tools export --input-path visual/CoreBiom-all.qzv --output-path export/visual/CoreBiom-all
+
+
+
+###### Biom convert
+
+# Aim: Convert to/from the BIOM table format
+
+biom convert -i export/core/RarTable-all/feature-table.biom -o export/core/RarTable-all/table-from-biom.tsv --to-tsv
+
+ # Aim: Remove first line and rename '#OTU ID' into 'ASV'
+
+ sed '1d ; s/\#OTU ID/ASV_ID/' export/core/RarTable-all/table-from-biom.tsv > export/core/RarTable-all/ASV.tsv
+
+
+###############################################################
+### For Bacteria
+###############################################################
+
+cd $DATADIRECTORY_16S
+
+eval "$(conda shell.bash hook)"
+conda activate qiime2-2021.4
+
+# Aim: Filter sample from table based on a feature table or metadata
+
+ qiime feature-table filter-samples \
+        --i-table core/RarTable.qza \
+        --m-metadata-file $METADATA_ITS/sample-metadata.tsv \
+        --p-where "[#SampleID] IN ('bark_foret_nord_01', 'bark_foret_nord_02', 'bark_foret_nord_03', 'base_palm_creek_pernod', 'leaf_creek_pernod', 'leaf_nursery_01', 'leaf_nursery_02', 'leaf_PPRB_01', 'leaf_PPRB_02', 'root_creek_pernod')"  \
+        --o-filtered-table core/RarTable-all.qza
+           
+           
+# Aim: Identify "core" features, which are features observed,
+     # in a user-defined fraction of the samples
+
+qiime feature-table core-features \
+        --i-table core/RarTable-all.qza \
+        --p-min-fraction 0.1 \
+        --p-max-fraction 1.0 \
+        --p-steps 10 \
+        --o-visualization visual/CoreBiom-all.qzv
+        
+qiime tools export --input-path core/RarTable-all.qza --output-path export/core/RarTable-all    
+        
+#qiime tools export --input-path visual/CoreBiomAll.qzv --output-path export/visual/CoreBiomAll
+qiime tools export --input-path visual/CoreBiom-all.qzv --output-path export/visual/CoreBiom-all
+
+
+###### Biom convert
+
+# Aim: Convert to/from the BIOM table format
+
+biom convert -i export/core/RarTable-all/feature-table.biom -o export/core/RarTable-all/table-from-biom.tsv --to-tsv
+
+ # Aim: Remove first line and rename '#OTU ID' into 'ASV'
+ 
+ sed '1d ; s/\#OTU ID/ASV_ID/' export/core/RarTable-all/table-from-biom.tsv >export/core/RarTable-all/ASV.tsv
